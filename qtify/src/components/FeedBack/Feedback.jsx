7ce3fd041a74e80bfcb65cf1c46d90f6edb3c4fb
Nithin <nithinappu963@gmail.com>
2024-04-22 21:28:09 +0530

    All changes done in NavBar

--- qtify/src/components/FeedBack/Feedback.jsx
+++ qtify/src/components/FeedBack/Feedback.jsx
@@ -1,3 +1,4 @@
+import '../FeedBack/Feedback.css'
 export default function Feedback(){
     return(
         <button className='feed-btn'>Give Feedback</button>
--- qtify/src/components/SearchInput/SearchInput.jsx
+++ qtify/src/components/SearchInput/SearchInput.jsx
@@ -1,21 +1,15 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Search from '@mui/icons-material/Search';
 import SearchIcon  from '../../assets/search-icon.svg';
 import '../SearchInput/SearchInput.css'
 
 export default function SearchInput(){
     return(
-        <Box>
-            <TextField
-            size='small'
-            className='searchInput'
-            placeholder="Search a song of your choice"
-            variant="outlined"
-            />
-            <button className='search-btn'> <img src={SearchIcon} alt="" />
+        
+        <div>
+         <input className='searchInput' placeholder='Search a song of your choice' />
+            <button className='search-btn'>
+                <img src={SearchIcon} alt="" />
             </button>
-            </Box>
+        </div>
     )
 };
