04e2164b6ce94b6eb88085a151b9f95e26e4aa71
Nithin <nithinappu963@gmail.com>
2024-04-26 19:48:05 +0530

    newly dohne 26/4 trial 2

--- qtify/src/Components/Hero/hero.jsx
+++ qtify/src/Components/Hero/hero.jsx
@@ -1,24 +1,17 @@
-import './hero.css';
-import heroImg from "../../assets/hero_headphones.png";
-
-
-
-
-function Hero() {
-    return (
-      <>
-        <div className='bannner'>
-            <div className='text'>
+import React from "react"
+import '../Hero/hero.css'
+import heroBanner from '../../assets/hero_headphones.png'
+export default function Hero(){
+    return(
+       <>
+        <div className="heroBanner">
+        <div className='text'>
                 <p align="center">100 Thousand Songs, ad-free</p>
                 <p align="center">Over thousands podcast episodes</p>
             </div>
-            <div className='image'>
-                <img src={heroImg} alt="hero" className='heroImage' />
-            </div>
+            <img src={heroBanner} className="heroImage" alt="Hero-Banner" />
         </div>
-      </>
-    )
-  }
-  
-  export default Hero;
-  
+
+       </>
+       )
+}
